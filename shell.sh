47318561e5f5ede7dc7155fc35c80f62ba7d2238
vamsibyramala <70326727+vamsibyramala@users.noreply.github.com>
2024-07-30 22:04:31 +0530

    Delete shell.sh

--- shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-repo_url="https://njdoxqmycr7yhui4f5lvzzpxqn2obfowuwfmjf2nk5jlluyj6mka@dev.azure.com/vamsi8188/project/_git/project"
-local_dir="/tmp/temp"
-git clone $repo_url $local_dir
-cd $local_dir
-sed -i "s|image:.*|image: vamsi.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
-git add .
-git commit -m "manifest file updated"
-git push
